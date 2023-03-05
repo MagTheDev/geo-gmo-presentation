5e4d7c0c18a61506b1121d9df6d33b878b0d8091
MagTheDev <jtulek@at-rt.com>
2023-03-05 21:56:25 +0100

    Add basic explanation structure

---
+++ components/Basics.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1> Základy </h1>
+        <div>
+            <ul>
+                <li>
+                    GMO - Geneticky Modifikované Organizmy
+                </li>
+                <li>
+                    Genetická informácia bola pozmenená - zlepšenie vlastností
+                </li>
+                <li>
+                    Ako pokračovanie tradičného šľachtenia
+                </li>
+                <li>
+                    Dokážeme dosiahnuť vlastnosti, ktoré sa v prírode nevyskytujú
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
