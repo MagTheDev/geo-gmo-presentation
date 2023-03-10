a548d0be037a982114367b6eda309e10535b1e06
Your Name <you@example.com>
2023-03-10 12:52:08 +0100

    Add map to EU stance

--- components/EuStance.vue
+++ components/EuStance.vue
@@ -7,16 +7,26 @@
                 Postoj EU ku GMO
             </h1>
             <ul class="text-3xl list-outside list-disc mx-8">
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 1 ? '0' : '1' }">Pestovanie a predávanie iba s povolením</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 2 ? '0' : '1' }">Eú prideluje pre všetky členské krajiny</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 3 ? '0' : '1' }">Majú právo zakázať ich pestovanie na svojom území</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 4 ? '0' : '1' }">Momentálne sa pestuje iba modifikovaná kukurica</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 5 ? '0' : '1' }">Predávať a dovážať je možné ďalších 58 rastlín</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 6 ? '0' : '1' }">Väčšina GMO sa používa na výrobu krmiva, môžu byť aj súčasťou potravín</li>
-                <li class="mb-4 transition-opacity" :style="{ opacity: step < 7 ? '0' : '1' }">Ak výrobok obsahuje &gt;0.9% GMO, musí byť označené</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 1 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Pestovanie a predávanie iba s povolením</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 2 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Eú prideluje pre všetky členské krajiny</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 3 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Majú právo zakázať ich pestovanie na svojom území</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 4 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Momentálne sa pestuje iba modifikovaná kukurica</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 5 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Predávať a dovážať je možné ďalších 58 rastlín</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 6 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Väčšina GMO sa používa na výrobu krmiva, môžu byť aj súčasťou potravín</li>
+                <li class="mb-4 transition-opacity" :style="{ opacity: step < 7 ? '0' : '1', display: step > 7 ? 'none' : 'block' }">Ak výrobok obsahuje &gt;0.9% GMO, musí byť označené</li>
             </ul>
+            <div v-if="step > 7" class="z-10 relative" >
+                <NuxtImg src="/eu_gmo.svg" />
+                <div>
+                    <div class="w-5 h-5 rounded bg-[#AE9c6E]"></div>
+                    <span></span>
+                </div>
+                <div>
+                    <div class="w-5 h-5 rounded bg-[#86B742]"></div>
+                    <span></span>
+                </div>
+            </div>
         </div>
-
     </div>
 
 </template>
@@ -25,7 +35,7 @@
 import { onKeyUp } from '@vueuse/core';
 
 
-const maxSteps = 7
+const maxSteps = 8
 
 const step = ref(0)
 
